b7726e3b33938247437663d561bdaaf1f8033e5a
nonuserfrommars <condesn99@gmail.com>
2022-12-11 12:02:08 -0300

    cambios pre entrega 2

--- app.js
+++ app.js
@@ -1,19 +1,99 @@
+const contenidoShop = document.getElementById("contenidoShop");
+
 const productos= [
-  {nombre: "harina", precio: 150},
-  {nombre: "leche", precio: 140},
-  {nombre: "azucar", precio: 160},
-  {nombre: "manteca", precio: 400},
-  {nombre: "aceite", precio: 180},
-  {nombre: "levadura", precio: 100},
-  {nombre: "sal", precio: 100},
-  {nombre: "chocolate", precio: 500},
-  {nombre: "dulce de leche", precio: 300},
-  {nombre: "vainilla", precio: 150},
-  {nombre: "huevos", precio: 380},
+  { 
+    id:1,
+    nombre: "harina",
+    precio: 150,
+    img: "img/harina.jpg",
+  },
+  {
+    id:2,
+    nombre: "leche",
+    precio: 140,
+    img: "img/leche.jpg",
+  },
+  {
+    id:3,
+    nombre: "azucar",
+    precio: 160,
+    img: "img/azucar.jpg",
+  },
+  {
+    id:4,
+    nombre: "manteca",
+    precio: 400,
+    img: "img/manteca.jpg",
+  },
+  {
+    id:5,
+    nombre: "aceite",
+    precio: 180,
+    img:"img/aceite.jpg",
+  },
+  {
+    id:6,
+    nombre: "levadura", 
+    precio: 100,
+    img:"img/levadura.jpg",
+  },
+  {
+    id:7,
+    nombre: "sal",
+    precio: 100,
+    img:"img/sal.jpg",
+  },
+  {
+    id:8,
+    nombre: "chocolate",
+    precio: 500,
+    img: "img/chocolate.jpg",
+  },
+  {
+    id:9,
+    nombre: "dulce de leche", 
+    precio: 300,
+    img:"img/ddl.jpg",
+  },
+  {
+    id:10,
+    nombre: "vainilla", 
+    precio: 150,
+    img:"img/vainilla.jpg",
+  },
+  {
+    id:11,
+    nombre: "huevos", 
+    precio: 380,
+    img: "img/huevos.jpg",
+  },
 ];
+
 let carrito = []
 
-let seleccion = prompt("Hola! Bienvenido a Market Shop. ¿Quiere comprar algun producto?")
+productos.forEach((product)=> {
+  let content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = ` 
+  <img src="${product.img}">
+  <h3>${product.nombre}</h3>
+  <p class="price">${product.precio} $</p>
+  `;
+
+contenidoShop.append(content);
+
+let comprar = document.createElement("button");
+comprar.innerText= "comprar";
+comprar.className= "comprar";
+
+content.append(comprar);
+
+});
+
+
+
+
+let seleccion = prompt("Hola! Bienvenid@ a Market Shop. ¿Quiere comprar algun producto?")
 while(seleccion != "si" && seleccion !="no"){
   alert("por favor ingresar si o no")
   seleccion = prompt("¿Quiere comprar algun producto? si o no")
@@ -90,4 +170,4 @@ while(seleccion != "no"){
 
 const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
 console.log(`El total a pagar es: ${total}`);
-alert(`El total a pagar es: $${total} ¡Gracias y hasta luego!`);
+alert(`El total a pagar es: $${total} ¡Gracias por visitarnos! Saludos`);
